650137748b47a382a724927333058a162d553605
smithdws1 <smithdws@hotmail.com>
2024-05-07 22:32:41 -0600

    Finish Prep for Team gathering

---
+++ test_address.py
@@ -0,0 +1,11 @@
+from address import extract_city, \
+    extract_state, extract_zipcode
+import pytest
+
+def test_extract_city():
+
+def test_extract_state():
+
+def test_extract_zipcode():
+
+pytest.main(["-v", "--tb=line", "-rN", __file__])
